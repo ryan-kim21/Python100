7a84732ed8f0c96a86576944dbac46091d4ebbfc
ryan-kim21 <63631441+ryan-kim21@users.noreply.github.com>
2023-01-12 22:41:59 +0900

    day2

---
+++ day2.py
@@ -0,0 +1,67 @@
+# print("welcome to the rollercoaster!")
+# height = int(input("What is your height in cm?"))
+
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
+########################################################################
+
+# bmi = round(weight / height ** 2)
+# if bmi < 18.5:
+#     print(f"your bmi is {bmi}")
+# elif bmi < 25:
+#     print(f"your bmi is {bmi}")
+
+# else:
+#     print(f"your bmi is {bmi}")
+
+########################################################################
+
+# print("Welcome to Python Pizza Deliveries!")
+# size = input("What size pizza do you want? S, M, or L")
+# add_pepperoni = input("Do you want pepperoni? Y or N")
+# extra_cheese = input("Do you want extra cheese? Y or N")
+
+# bill = 0
+# if size == "S":
+#     bill += 15
+# elif size == "M":
+#     bill += 20
+# else :
+#     bill += 25
+
+# if add_pepperoni == "Y" :
+#     if size == "S":
+#         bill += 2
+#     else:
+#         bill += 3
+# if extra_cheese == "Y":
+#     bill += 1
+
+# print(f"your final bill is ${bill}")
+
+
+########################################################################
+
+
+
+########################################################################
+
+
+########################################################################
+
+
+########################################################################
+
+
+########################################################################
+
+
+########################################################################
+
+
+########################################################################
+
+
+########################################################################
