d3ef62a4137ddd878040b6826ef9f024e570179a
ryan-kim21 <63631441+ryan-kim21@users.noreply.github.com>
2023-01-14 22:22:22 +0900

    day3

--- day1.py
+++ day1.py
@@ -45,20 +45,20 @@
 
 # bmi = int(weight)  / float(height) ** 2 
 # bmi_as_int = int(bmi)
-# print(bmi)
-####################################
+# # print(bmi)
+# ####################################
 
-print("Welcome to the tip calculator.")
-bill = float(input("what was the total bill?$"))
-tip = int(input("Hw much tip would you like to give? 10, 12 or 15"))
-people = int (input("How many people to split the bill?"))
-tip_as_percent = tip /100
-total_tip_amount= bill * tip_as_percent
-total_bill = bill + total_tip_amount
-bill_per_person = total_bill /people
-final_amount = round(bill_per_person, 2)
-final_amount = "{:.2f}".format(bill_per_person)
-print(f"Each person should pay: ${final_amount}")
+# print("Welcome to the tip calculator.")
+# bill = float(input("what was the total bill?$"))
+# tip = int(input("Hw much tip would you like to give? 10, 12 or 15"))
+# people = int (input("How many people to split the bill?"))
+# tip_as_percent = tip /100
+# total_tip_amount= bill * tip_as_percent
+# total_bill = bill + total_tip_amount
+# bill_per_person = total_bill /people
+# final_amount = round(bill_per_person, 2)
+# final_amount = "{:.2f}".format(bill_per_person)
+# print(f"Each person should pay: ${final_amount}")
 
 
 
---
+++ day3.py
@@ -0,0 +1,101 @@
+# import random 
+
+
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+
+# #0.000000000 ~ 0.99999999....
+# random_float = random.random()
+# print(random_float)
+
+# love_socre = random.randint(1, 100)
+# print(f"ffff {love_socre}")
+
+####################################
+
+
+# import random
+# random.randint(0, 1)
+
+# if random_side == 1:
+#     print("Heads")
+# else:
+#     print("Tails")
+
+####################################
+
+
+# states_of_america = ["Delaware", "Pennyivvania", "New jersey"]
+
+# states_of_america.extend(["test1", "test2"])
+
+# print (states_of_america)
+
+
+####################################
+
+# #Split string method
+# names_string = input ("give me everybody's names, seperated by a comma.")
+
+# names = names_string.split(",")
+
+# print(names)
+
+####################################
+
+# import random
+# names_string = input("name")
+# names =names_string.split(",")
+
+# num_items = len(names)
+
+# random_choice = random.randint(0, num_items - 1)
+# person_who_will_pay = names[random_choice]
+
+# print (person_who_will_pay + " is going to buy the meal today.")
+
+
+####################################
+
+# fruits = ["str","nec","app","gra","pea","cher"]
+
+# vegetables = ["spi","kal","tom","cel","pot"]
+
+# dirty_dozen = [fruits, vegetables]
+
+# print(dirty_dozen)
+####################################
+
+# row1 = ["■","■","■"]
+# row2 = ["■","■","■"]
+# row3 = ["■","■","■"]
+# map = [row1, row2, row3]
+
+# print (f"{row1}\n{row2}\n{row3}")
+
+# position = input("Where do you want to put the treasure?")
+
+# horizonal = int(position[0])
+# vertical = int(position[1])
+
+# seleted_row = map[vertical-1]
+# seleted_row[horizonal -1 ] = "X"
+
+# print (f"{row1}\n{row2}\n{row3}")
+
+####################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
