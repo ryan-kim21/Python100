7bb33208a62aa56ff67fd2e1b90db6366673cfce
ryan-kim21 <63631441+ryan-kim21@users.noreply.github.com>
2023-01-17 21:18:33 +0900

    day6

---
+++ day5.py
@@ -0,0 +1,13 @@
+# def my_function():
+#     print("Hello")
+#     print("Bye")
+# my_function()
+
+# ####################################
+
+
+# number_of_hurdles =6
+# while number_of_hurdles > 0:
+   
+#     number_of_hurdles-=1
+#     print(number_of_hurdles)
---
+++ day6.py
@@ -0,0 +1,45 @@
+# word_list = ["ardvark","baboon","camel"]
+
+# import random
+# chosen_word = random.choice(word_list)
+
+
+# print(f'Passt, the solution is {chosen_word}.')
+
+# guess = input("Guess a letter:").lower()
+
+# for letter in chosen_word:
+#     if letter == guess:
+#         print("right")
+#     else:
+#         print("Wrong")
+    
+
+# word_list = ["ardvark","baboon","camel"]
+
+# import random
+# chosen_word = random.choice(word_list)
+
+
+# print(f'Passt, the solution is {chosen_word}.')
+
+# display =[]
+# for letter in range(len(chosen_word)):
+#     display += "_"
+# print(display)
+
+# word_length = len(chosen_word)
+# for _ in range(word_length):
+#     display += "_"
+# print(display)
+
+
+# guess = input("Guess a letter:").lower()
+
+# for position in range(word_length):
+#     letter = chosen_word[position]
+#     if letter == guess:
+#         display[position] = letter
+
+# print(display)
+    
