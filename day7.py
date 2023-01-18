0b0729d33a2e869a0260d93405c2751a318c69d7
ryan-kim21 <63631441+ryan-kim21@users.noreply.github.com>
2023-01-18 23:46:48 +0900

    day7

---
+++ day7.py
@@ -0,0 +1,26 @@
+# def greet():
+#     print("hello")
+#     print("How do you do?")
+#     print("Isn't the weather nice today?")
+
+# greet()
+
+
+# def greet_with_name(name):
+#     print(f"Hellow{name}")
+#     print("how do dyou do {name}?")
+
+# greet_with_name()
+
+
+#Fuctions with more than 1 input
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"what is it like in {location}")
+
+greet_with("jack bauer","nowhere")
+greet_with("nowhere","jeckbauer")
+
+
+greet_with(name="Angela",location="London")
+
