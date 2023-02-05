4cb7599d2dd5a66c0a6a27a52a8fc8fe12208fdf
ryan-kim21 <63631441+ryan-kim21@users.noreply.github.com>
2023-02-05 18:29:45 +0900

    day25

--- day24.py
+++ day24.py
@@ -2,6 +2,8 @@ from fpdf import FPDF
 import pandas as pd 
 
 pdf = FPDF(origentation = "P", unit="mm", format="A4")
+pdf.set_auto_page_preak(auto=False, margin=0)
+
 
 df = pd.read_csv("data.csv")
 
@@ -12,6 +14,19 @@ for index, row in df.iterrows():
     pdf.cell(w=0, h=12, txt=row["Topic"],align="L", ln=1, border=1)
 
     pdf.line(10,17,200,17)
+
+    pdf.ln(278)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align ="R")
+
+    for i in range(row["Pages"]-1):
+        pdf.add_page()
+
+        #Set the footer
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align ="R")
 
 
 
