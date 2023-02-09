b0d8e8de3c95dc761f8cf154217e5f8125d05446
ryan-kim21 <63631441+ryan-kim21@users.noreply.github.com>
2023-02-09 22:07:01 +0900

    day36

--- day33.py
+++ day33.py
@@ -10,7 +10,7 @@ driver = webdriver.Chrome()
 driver.implicitly_wait(5)
 
 
-url="http://www.pixabay.com/ko/images/search/사과"
+url="http://www.pixabay.com/ko/images/search/사과/"
 driver.get(url=url)
 
 
@@ -18,7 +18,9 @@ image_xpath = "/html/body/div[1]/div[2]/div/div[3]/div/div[3]/div[1]/div/div/div
 image_url = driver.find_element(By.XPATH, image_xpath).get_attribute('src')
 print("image_url:", image_url)
 
-image_byte = Request(image_url, headers={'User-Agent:' : 'Mozilla/5.0'})
-f =open('apple.jgp', "wb")
+image_byte = Request(image_url, headers={'User-Agent:': 'Mozilla/5.0'})
+f =open('apple.jpg', "wb")
 f.write(urlopen(image_byte).read())
 f.close()
+
+sleep(30)
---
+++ day34.py
@@ -0,0 +1,42 @@
+#여러개 이미지 받아오기
+from selenium import webdriver
+import chromedriver_autoinstaller
+from time import sleep
+from selenium.webdriver.common.by import By
+from urllib.request import Request, urlopen
+
+
+chromedriver_autoinstaller.install()
+driver = webdriver.Chrome()
+driver.implicitly_wait(5)
+
+
+url="https://pixabay.com/ko/images/search/사과/"
+driver.get(url=url)
+
+#finde(), find_all(), .find_elements
+# image_xpath = "/html/body/div[1]/div[2]/div/div[3]/div/div[3]/div[1]/div/div/div/a/img"
+
+image_area_xpath = "/html/body/div[1]/div[2]/div/div[3]/div/div[3]"
+image_area = driver.find_element(By.XPATH, image_area_xpath)
+image_elements = image_area.find_elements(By.TAG_NAME, "img")
+    # .get_attribute('src')
+
+image_urls=[]
+
+for image_element in image_elements :
+    image_url = ""
+    if image_element.get_attribute('data-lazy') is None:
+        image_url = image_element.get_attribute("src")
+    else:
+        image_url = image_element.get_attribute("data-lazy")
+    image_urls.append(image_url)
+
+for i in range(len(image_urls)):
+    image_url =image_urls[i]
+    image_byte = Request(image_url, headers={'User-Agent': 'Mozilla/5.0'})
+    f = open(f"apple{i}.jpg", "wb")
+    f.write(urlopen(image_byte).read())
+    f.close()
+
+# sleep(30)
