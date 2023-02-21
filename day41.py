baaf59a224d33cd090fcc2755386bbfe0c301d56
ryan-kim21 <63631441+ryan-kim21@users.noreply.github.com>
2023-02-21 23:47:39 +0900

    day41

--- day41.py
+++ day41.py
@@ -1,16 +1,128 @@
-#Functions with Outputs
-def format_name(f_name, l_name):
+# #Functions with Outputs
+# def format_name(f_name, l_name):
     
-    if f_name ==""or l_name == "":
+#     if f_name ==""or l_name == "":
 
-        return 
-        formated_f_name = f_name.title()
-        formated_l_name = l_name.title()
+#         return 
+#         formated_f_name = f_name.title()
+#         formated_l_name = l_name.title()
 
-    return f"Result: {formated_f_name} {formated_l_name}"
+#     return f"Result: \{formated_f_name} {formated_l_name}"
     
-print(format_name(input("What is your first name?"))),
-input("What is your last name?")
+# print(format_name(input("What is your first name?"))),
+# input("What is your last name?")
+
+
+
+
+# def days_in_month(year, month):
+#     month_days = [31, 28, 31, 30, 31, 30, 31]
+#     if is_leap(year) and month == 2:
+#         return 29
+#     return month_days[month - 1]
+# year = int(input("enther a year:"))
+
+# while treeHit < 10:
+#     attack=treeHit()
+#     print(input(f"몇번 때릴까요?{attack}"))
+#     treeHit = treeHit +1
+#     print("나무를 %d 번 찍음" % treeHit)
+#     if treeHit == 10:
+#         print("나무가 넘어갑니다.")
+
+
+
+# add = 0
+# for i in range(1, 11):
+#     add = add + i
+#     print(add)
+
+
+# marks = [90, 25, 67, 45, 80]
+# for number in range(len(marks)): #요소안 갯수를 돌려주는 함수
+#     if marks[number] < 60: 
+#         continue
+#     print("%d번 학생 축하합니다. 합격입니다." % (number+1))
+
+
+# for i in range(2, 10):
+#     for j in range(1, 10):
+#         print(i*j, end="")
+#     print('')
+
+
+
+# a = [1,2,3,4]
+# result = []
+# for num in a:
+#   result.append(num*3)
+
+# print(result)
+
+
+# def add(a, b):
+#     print("%d, %d 합은 %d" %(a,b,a+b))
+    
+# # add(3,4)
+
+
+# def add_many(*args):
+#     result = 0
+#     for i in args:
+#         result = result + i
+#     return result
+
+# result = add_many(1,2,4,78)
+
+# print(result)
+
+# add = lambda a, b : a+b
+# result = add(3,4)
+# print(result)
+
+# result = 0
+# result1 = 0
+
+# def add1(num):
+#     global result
+#     result += num
+#     return result
+# print(add1(3))
+# print(add1(4))
+
+# def add2(num):
+#     global result
+#     result += num
+#     return result
+# print(add2(3))
+# print(add2(4))
+
+
+# class Calculator:
+#     def __init__(self):
+#         self.result = 0
+
+#     def add(self, num):
+#         self.result += num
+#         return self.result
+
+# cal1 = Calculator()
+# cal2 = Calculator()
+
+# print(cal1.add(3))
+# print(cal1.add(4))
+# print(cal2.add(3))
+# print(cal2.add(7))
+
+
+
+
+class FourClal
+
+
+
+
+
 
 
 
